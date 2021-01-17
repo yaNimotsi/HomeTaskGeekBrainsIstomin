c102d1d09155c67c8e4e02fe8ef7308c004e613d
yaNimotsi <yanimotsi@yandex.ru>
2021-01-17 17:04:53 +0300

    Add project from another folder

---
+++ Tsak1ForLesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsak1ForLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dToday = DateTime.Now.ToString("D");
+            Console.Write("Укажите свое имя: ");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Привет {0}, сегодня {1}",userName, dToday );
+            Console.ReadLine();
+        }
+    }
+}
